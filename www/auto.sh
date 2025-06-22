679eee15c7367f3d1d64a12595b000714968bc79
Your Name <you@example.com>
2025-06-23 02:14:18 +0800

    update

--- www/auto.sh
+++ www/auto.sh
@@ -3,6 +3,7 @@
 # auto update
 SCRIPT_PATH="$(realpath "$0")"\
 SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+echo path:$SCRIPT_PATH
 echo path:$SCRIPT_DIR
 echo "update" >> $SCRIPT_DIR/README.md
 git add .
