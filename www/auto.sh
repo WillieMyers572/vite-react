c77fe541aa618022c3783ac0dd6c41cee703826e
Your Name <you@example.com>
2025-06-23 02:19:59 +0800

    update

--- www/auto.sh
+++ www/auto.sh
@@ -1,11 +1,28 @@
 #!/bin/bash
 
 # auto update
-SCRIPT_PATH="$(realpath "$0")"
-SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
-echo path:$SCRIPT_PATH
-echo path:$SCRIPT_DIR
-echo "update" >> $SCRIPT_DIR/README.md
-git add .
-git commit -m 'update'
-git push
+auto_update(){
+    SCRIPT_PATH="$(realpath "$0")"
+    SCRIPT_DIR="$(dirname "$SCRIPT_PATH")"
+    echo path:$SCRIPT_DIR
+    echo "update" >> $SCRIPT_DIR/README.md
+    git add .
+    git commit -m 'update'
+    git push
+}
+
+count=10 
+i=0
+
+while [ $i -lt $count ]; do
+    # 获取当前时间并格式化为 mmddss
+    current_time=$(date +"%m%d%S")
+    echo "$current_time"
+    auto_update
+    # 生成一个随机的间隔时间（0 到 60 秒之间）
+    sleep_time=$((RANDOM % 60))
+    sleep "$sleep_time"  # 随机间隔时间
+
+    # 增加计数器
+    ((i++))
+done
